b71b30bcba7a5a8fa20042abc2447458100b0ad5
zhaoyun <zhaoyun2316@gmail.com>
2019-05-26 16:27:07 +0800

    leetcode 204: 计算小于该值的素数个数

---
+++ src/main/java/com/zhaoyun/leetcode/math/LT204.java
@@ -0,0 +1,33 @@
+package com.zhaoyun.leetcode.math;
+
+/**
+ * 计算小于该值的素数个数
+ *
+ * Created by ZhaoYun on 2019-05-26
+ **/
+public final class LT204 {
+    public int countPrimes(int n) {
+        int count = 0;
+
+        if (n <= 1) {
+            return count;
+        }
+
+        for (int i = 2; i < n; i++) {
+            boolean f = true;
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0 && j != i) {
+                    f = false;
+                    break;
+                }
+            }
+            if (f) count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LT204().countPrimes(499979));
+    }
+}
