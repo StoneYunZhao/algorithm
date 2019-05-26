e6484346484aff3442fb7d0f1988f7ea0674e3c4
zhaoyun <zhaoyun2316@gmail.com>
2019-05-26 15:39:17 +0800

    leetcode 100: 判断二叉树是否相同

---
+++ src/main/java/com/zhaoyun/leetcode/tree/LT100.java
@@ -0,0 +1,22 @@
+package com.zhaoyun.leetcode.tree;
+
+/**
+ * Created by ZhaoYun on 2019-05-26
+ **/
+public final class LT100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
