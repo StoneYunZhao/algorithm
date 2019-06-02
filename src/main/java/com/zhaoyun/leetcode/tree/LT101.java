2b4ffa1f19c7e345276197604d21f58987a47637
zhaoyun <zhaoyun2316@gmail.com>
2019-06-02 12:30:28 +0800

    leetcode 101： 判断树是否对称

---
+++ src/main/java/com/zhaoyun/leetcode/tree/LT101.java
@@ -0,0 +1,24 @@
+package com.zhaoyun.leetcode.tree;
+
+/**
+ * 判断树是否对称
+ * <p>
+ * Created by ZhaoYun on 2019-06-02
+ **/
+public final class LT101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+
+        return left.val == right.val && isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+    }
+}
