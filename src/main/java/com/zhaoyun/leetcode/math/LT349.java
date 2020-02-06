bb7044668beefa4acf9ab40e490d1e5486bbe635
zhaoyun <zhaoyun2316@gmail.com>
2020-02-06 19:18:13 +0800

    LeetCode 349：两个数组求交集

---
+++ src/main/java/com/zhaoyun/leetcode/math/LT349.java
@@ -0,0 +1,38 @@
+package com.zhaoyun.leetcode.math;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 两个数组求交集
+ *
+ * @author zhaoyun
+ * @Date: 2020/2/6
+ */
+public final class LT349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> c = new HashMap<>();
+        Set<Integer> res = new HashSet<>();
+
+        for (int i : nums1) {
+            c.put(i, 1);
+        }
+
+        for (int i : nums2) {
+            if (c.get(i) != null) {
+                res.add(i);
+            }
+        }
+
+        int n = res.size();
+        int[] r = new int[n];
+        int i = 0;
+        for (int j : res) {
+            r[i++] = j;
+        }
+        return r;
+    }
+}
+
