cf2389c1da0557260c9055f07d88ae8211db39be
zhaoyun <zhaoyun2316@gmail.com>
2020-02-06 11:40:09 +0800

    LeetCode 274： h 指数计算，O(n) 的算法

--- src/main/java/com/zhaoyun/leetcode/sort/LT274.java
+++ src/main/java/com/zhaoyun/leetcode/sort/LT274.java
@@ -9,6 +9,9 @@ import java.util.Arrays;
  * @Date: 2020/2/5
  */
 public final class LT274 {
+    /**
+     * 先排序
+     */
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
 
@@ -23,6 +26,29 @@ public final class LT274 {
 
         for (int h = n - 1; h > 0; h--) {
             if (citations[n - h] >= h && citations[n - h - 1] <= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * 桶排序的方式，这位天才的思路：
+     * https://leetcode.com/problems/h-index/discuss/70768/Java-bucket-sort-O(n)-solution-with-detail-explanation
+     */
+    public int hIndex2(int[] citations) {
+        int n = citations.length;
+        int[] buckets = new int[n + 1];
+
+        for (int i =0; i< n; i++) {
+            buckets[Math.min(n, citations[i])]++;
+        }
+
+        int sum = 0;
+        for (int h = n; h >= 0; h--) {
+            sum += buckets[h];
+            if (sum >= h) {
                 return h;
             }
         }
