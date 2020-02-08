83705c36870ba1a5cffb745941042afcbe362182
zhaoyun <zhaoyun2316@gmail.com>
2020-02-08 13:36:03 +0800

    LeetCode 501：二叉查找树中找到出现次数最多的元素

---
+++ src/main/java/com/zhaoyun/leetcode/tree/LT501.java
@@ -0,0 +1,51 @@
+package com.zhaoyun.leetcode.tree;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 二叉查找树中找到出现次数最多的元素
+ *
+ * @author zhaoyun
+ * @Date: 2020/2/7
+ */
+public final class LT501 {
+    /**
+     * 把所有元素放在 map 中，然后找到最大出现次数的元素
+     */
+    public int[] findMode(TreeNode root) {
+        Map<Integer, Integer> map = new HashMap<>();
+        addToMap(root, map);
+
+        int max = Integer.MIN_VALUE;
+        Set<Integer> res = new HashSet<>();
+        for (Map.Entry<Integer, Integer> e: map.entrySet()) {
+            if (e.getValue() > max) {
+                max = e.getValue();
+                res.clear();
+                res.add(e.getKey());
+            } else if (e.getValue() == max) {
+                res.add(e.getKey());
+            }
+        }
+
+        int[] r = new int[res.size()];
+        int cur = 0;
+        for (int i: res) {
+            r[cur++] = i;
+        }
+        return r;
+    }
+
+    private void addToMap(TreeNode node, Map<Integer, Integer> map) {
+        if (node == null) {
+            return;
+        }
+
+        map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+        addToMap(node.left, map);
+        addToMap(node.right, map);
+    }
+}
