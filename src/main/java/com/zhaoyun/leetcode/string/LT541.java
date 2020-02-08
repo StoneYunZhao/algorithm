4e52028035a2f45664f855ddd28ab9d313318277
zhaoyun <zhaoyun2316@gmail.com>
2020-02-08 13:51:16 +0800

    LeetCode 541：一个字符串，每隔 2k 个字符，前面 k 个字符反转，后面 k 个字符保持原来的顺序

---
+++ src/main/java/com/zhaoyun/leetcode/string/LT541.java
@@ -0,0 +1,35 @@
+package com.zhaoyun.leetcode.string;
+
+/**
+ * 一个字符串，每隔 2k 个字符，前面 k 个字符反转，后面 k 个字符保持原来的顺序
+ *
+ * @author zhaoyun
+ * @Date: 2020/2/8
+ */
+public final class LT541 {
+
+    public String reverseStr(String s, int k) {
+        if (s == null || k <= 1) return s;
+
+        StringBuilder res = new StringBuilder();
+
+        boolean reverse = true;
+        int cur = 0, index = 0;
+        for (char c: s.toCharArray()) {
+            if (cur == k) {
+                cur = 0;
+                reverse = !reverse;
+                index += k;
+            }
+
+            if (reverse) {
+                res.insert(index, c);
+            } else {
+                res.append(c);
+            }
+            cur++;
+        }
+
+        return res.toString();
+    }
+}
