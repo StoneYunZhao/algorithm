ab742d123b879c0cd2e30661997c0bbb43576803
zhaoyun <zhaoyun2316@gmail.com>
2020-05-19 20:29:43 +0800

    LeetCode395: 字符子串中所有字符出现次数都大于 k 的最长子串。

--- src/main/java/com/zhaoyun/leetcode/LT395.java
+++ src/main/java/com/zhaoyun/leetcode/LT395.java
@@ -5,4 +5,39 @@ package com.zhaoyun.leetcode;
  * @Date: 2020/5/19
  */
 public final class LT395 {
+
+    public static void main(String[] args) {
+        int longest = new LT395().longestSubstring("bbaaacbd", 3);
+        System.out.println(longest);
+    }
+
+    public int longestSubstring(String s, int k) {
+        if (s == null || s.length() == 0 || k > s.length()) {
+            return 0;
+        }
+        return longest(s.toCharArray(), 0, s.length() - 1, k);
+    }
+
+    private int longest(char[] cs, int left, int right, int k) {
+        if (right - left + 1 < k) {
+            return 0;
+        }
+
+        int[] count = new int[26];
+        for (int i = left; i <= right; i++) {
+            count[cs[i] - 'a']++;
+        }
+
+        boolean allk = true;
+        int max = 0, l = left;
+        for (int i = left; i <= right; i++) {
+            if (count[cs[i] - 'a'] < k) {
+                max = Math.max(max, longest(cs, l, i - 1, k));
+                l = i + 1;
+                allk = false;
+            }
+        }
+
+        return allk ? right - left + 1 : Math.max(max, longest(cs, l, right, k));
+    }
 }
